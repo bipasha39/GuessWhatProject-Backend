830bac2a4fa64efd5c43e536b6e777a80a381bd3
bipasha39 <physiobipasha39@gmail.com>
2021-06-17 12:25:14 +0200

    added all words through seeders

--- seeders/20210616131145-words.js
+++ seeders/20210616131145-words.js
@@ -23,7 +23,84 @@ module.exports = {
          value: 'Git Push',
          createdAt: new Date(),
           updatedAt: new Date()
-        }
+        },{
+          value: '!False',
+          createdAt: new Date(),
+           updatedAt: new Date()
+         },{
+          value: 'For Loop',
+          createdAt: new Date(),
+           updatedAt: new Date()
+         },{
+          value: 'While Loop',
+          createdAt: new Date(),
+           updatedAt: new Date()
+         },{
+          value: 'Parenthesis',
+          createdAt: new Date(),
+           updatedAt: new Date()
+         },{
+          value: 'Integer',
+          createdAt: new Date(),
+           updatedAt: new Date()
+         },{
+          value: 'Terminal',
+          createdAt: new Date(),
+           updatedAt: new Date()
+         },{
+          value: 'Props',
+          createdAt: new Date(),
+           updatedAt: new Date()
+         },{
+          value: 'Parameter',
+          createdAt: new Date(),
+           updatedAt: new Date()
+         },
+         {
+          value: 'Variable',
+          createdAt: new Date(),
+           updatedAt: new Date()
+         },
+
+         {
+         value: 'Array',
+         createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          value: 'Object',
+          createdAt: new Date(),
+           updatedAt: new Date()
+         },
+         {
+          value: 'React FrameWork',
+          createdAt: new Date(),
+           updatedAt: new Date()
+         },
+         {
+          value: 'CamelCase',
+          createdAt: new Date(),
+           updatedAt: new Date()
+         },
+         {
+          value: 'Return Value',
+          createdAt: new Date(),
+           updatedAt: new Date()
+         },
+         {
+          value: 'If statement',
+          createdAt: new Date(),
+           updatedAt: new Date()
+         },{
+          value: 'Main function',
+          createdAt: new Date(),
+           updatedAt: new Date()
+         },{
+          value: 'Argument',
+          createdAt: new Date(),
+           updatedAt: new Date()
+         }
+        
       ], {});
   },
 
