d5090d3c1203af62fe52fc04e366f7e059b6d487
bipasha39 <physiobipasha39@gmail.com>
2021-07-05 16:31:48 +0200

    changing port into 5000

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ app.get("/", (req, res) => {
   res.send("I'm here");
 });
 
-app.listen(3000, () => {
-  console.log(`Server wait you no http://localhost:3000`);
+app.listen(5000, () => {
+  console.log(`Server wait you no http://localhost:5000`);
 });
